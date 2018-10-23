f530262d7e576f2d77cc69ec345199a1001626e4
Blake Gotting and Tavish OKeefe <tavish@>
2018-10-23 13:29:31 -0700

    Initial commit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-
+$(document).ready(function(){
+  $("form#personality-test").submit(function(event){
+    event.preventDefault();
+    $("#personality-response").show();
+    $("input:checkbox[name=personality]:checked").each(function(){
+      var personalityCheck = $(this).val();
+      $("#personality-response").append(personalityCheck + "<br>");
+    });
+    $('#personality-test').hide();
+  });
 });
